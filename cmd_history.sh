4521c1617c8de6bbd217d5a4b4caff6ef137dc65
MarronTakue <marron_taku@icloud.com>
2025-06-15 12:55:19 +0900

    APIを追加

---
+++ cmd_history.sh
@@ -0,0 +1,7 @@
+# .zshrcを開く
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"' >> ~/.zshrc
+echo '[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"' >> ~/.zshrc
+
+# 新しい設定を反映
+source ~/.zshrc
